7a539cdc6a4dd6b61b40e92fe23132a1d184f16a
aveenzhou <aveenzhou@gmail.com>
2014-10-26 23:30:13 +0800

    add new code

--- smt_app/main.py
+++ smt_app/main.py
@@ -16,42 +16,42 @@ AUTH_URL="http://gw.api.alibaba.com/auth/authorize.htm"
 
 APP_KEY="1067520"
 APP_SECRET="PUpMcxJ5om"
-
 LOCAL_APP_URL="http://127.0.0.1:8080/index"
 
-#test
-#test2
-auth_args={
-           "client_id":APP_KEY,
-           "site":"aliexpress",
-           "redirect_uri":LOCAL_APP_URL,
-           "state":'test'
-           }
+TOKEN_URL="https://gw.api.alibaba.com/openapi/http/1/system.oauth2/getToken/"+APP_KEY
 
-
-def create_sign_str(args):
-    keys=args.keys()
-    sorted_keys=sorted(keys)
-    temp=[]
-    for key in sorted_keys:
-        temp.append(''.join((key,auth_args[key])))
+def get_alibba_auth_url():
+    auth_args={
+               "client_id":APP_KEY,
+               "site":"aliexpress",
+               "redirect_uri":LOCAL_APP_URL,
+               "state":'test'
+               }
     
-    sign_str=''.join(temp)
-    encry_sign_str=hmac.new(APP_SECRET, sign_str, digestmod=hashlib.sha1).hexdigest().upper() #hmac_sha1加密算法
-    return encry_sign_str
-
-auth_args['_aop_signature']=create_sign_str(auth_args)
-
-print auth_args
-
-def get_alibba_auth_url(req_args):
-
-    url= '?'.join((AUTH_URL,urllib.urlencode(req_args)))
+    def create_sign_str(args):
+        keys=args.keys()
+        sorted_keys=sorted(keys)
+        temp=[]
+        for key in sorted_keys:
+            temp.append(''.join((key,auth_args[key])))
+        
+        sign_str=''.join(temp)
+        encry_sign_str=hmac.new(APP_SECRET, sign_str, digestmod=hashlib.sha1).hexdigest().upper() #hmac_sha1加密算法
+        return encry_sign_str
+    
+    auth_args['_aop_signature']=create_sign_str(auth_args)
+    
+    url= '?'.join((AUTH_URL,urllib.urlencode(auth_args)))
     return url
 
+def get_token_by_code(code):
+    req_args={
+              "grant_type":"authorization_code",
+              "need_refresh_token":""
+              }
+    
 
-
-
+web.config.alibba_auth_url=get_alibba_auth_url()
 
 ########################################
 urls=(
@@ -61,15 +61,15 @@ urls=(
 
 class Auth(object):
     def GET(self):
-        redirect_url=get_alibba_auth_url(auth_args)
+        redirect_url=web.config.alibba_auth_url
         web.seeother(redirect_url, absolute=True)
     
 
 class Index(object):
     def GET(self):
         input= web.input()
-        code=input.get('code','') 
-        
+        code=input.get('code','') #临时授权码
+        return  code
 
 
 
