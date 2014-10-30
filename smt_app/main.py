4a77a87533578856fe88d6c30cbdfc27af144291
aveenzhou <aveenzhou@gmail.com>
2014-10-30 18:21:18 +0800

    完善获取库SMT存信息接口，优化界面

--- smt_app/main.py
+++ smt_app/main.py
@@ -27,23 +27,33 @@ TOKEN_URL="https://gw.api.alibaba.com/openapi/http/1/system.oauth2/getToken/"+AP
 LOCAL_APP_URL="http://127.0.0.1:8080/auth"
 ###################
 MONGODB={
-#            "DB_SERVER":'10.20.14.196',
-            "DB_SERVER":'192.168.1.103',
-#            "DB_PORT":28888,
-            "DB_PORT":27017,
+            "DB_SERVER":'10.20.14.196',
+#            "DB_SERVER":'192.168.1.103',
+            "DB_PORT":28888,
+#            "DB_PORT":27017,
             "DB_NAME":'smt_app_db',
             "DB_SMT_COLL":'smt_procucts_coll',
             "DB_USER":'aveen',
             "DB_ADMIN_PWD":'123456',
             "DB_PWD":'123',
-            "IS_AUTH":False
+            "IS_AUTH":True
          }
 ##########################
+def imageUrlFilter(url):
+    tmp=url.split('.')
+    f='.'.join(tmp[:-1])
+    b=tmp[-1]
+    return '.'.join([f,'summ',b])
+
 
 render = render_jinja(os.path.normpath(os.path.dirname(__file__) + '/template'), encoding='utf-8')
+render._lookup.filters['imageUrlFilter']=imageUrlFilter
 
 http=urllib3.PoolManager()
 web.cache=web.Storage()
+web.cache.sku_properid_datas=web.storage()
+web.cache.sku_properval_datas=web.storage()
+web.cache.cate_attr_datas=web.storage()
 def get_alibba_auth_url():
     auth_args={
                "client_id":APP_KEY,
@@ -123,7 +133,6 @@ def get_alidata_by_api(api_name,
         req_args_str=''.join(temp)
         sign_str=''.join((urlPath,req_args_str))
         encry_sign_str=hmac.new(APP_SECRET, sign_str, digestmod=hashlib.sha1).hexdigest().upper() #hmac_sha1加密算法
-
         return encry_sign_str
     
     req_args['_aop_signature']=create_sign_str()
@@ -166,8 +175,51 @@ def release_dbcontext():
 urls=(
       '/auth','Auth',
       '/index','Index',
-      '/get_smtproducts','SMTProducts'
+      '/get_smtproducts','SMTProducts',
+      '/test','Test'
       )
+
+
+class Test(object):
+    def GET(self):
+        if not web.config.get('token_data',None) or web.config.token_data.get('error',None):
+            raise web.seeother(web.config.alibba_auth_url, absolute=True)
+        
+        id=web.input().get("id",None)
+        type=web.input().get('type',None)
+        cateid=web.input().get('cateid',None)
+        access_token=web.config.token_data.access_token
+        if id:
+            
+            p_info=get_alidata_by_api(
+                                   "api.findAeProductById",
+                                   access_token,
+                                   productId=id
+                                   )
+            json_data=json.loads(p_info)
+            del json_data['detail']
+            return json_data
+        
+        if type=="products":
+            res_data=get_alidata_by_api(
+                               "api.findProductInfoListQuery",
+                                access_token,
+                                productStatusType="onSelling",
+                                currentPage=1 #默认为第一页
+                               )
+            json_data=json.loads(res_data)
+            return res_data
+        
+        if cateid:
+            res_data=get_alidata_by_api(
+                               "api.getAttributesResultByCateId",
+                                access_token,
+                                cateId=cateid,
+                               )
+            json_data=json.loads(res_data)
+            web.header('Content-type', 'text/html;charset=utf-8')
+            return res_data
+
 
 class Auth(object):
     def GET(self):        
@@ -189,7 +241,8 @@ class Auth(object):
         if web.config.token_data.get('error',None):
             raise web.seeother(web.config.alibba_auth_url, absolute=True)
         else:
-            raise web.seeother("/index")
+            
+            raise web.seeother('/index')
         
 
         
@@ -206,32 +259,37 @@ class SMTProducts(object):
     '''获取SMT上的产品列表,存入数据库
                 上架:onSelling ；下架:offline ；审核中:auditing ；审核不通过:editingRequired
     '''
+    
     def GET(self):
         if not web.config.get('token_data',None) or web.config.token_data.get('error',None):
             raise web.seeother(web.config.alibba_auth_url, absolute=True)
         
-        access_token=web.config.token_data.access_token
+        self.access_token=web.config.token_data.access_token
         res_data=get_alidata_by_api(
                            "api.findProductInfoListQuery",
-                            access_token,
+                            self.access_token,
                             productStatusType="onSelling",
                             currentPage=1 #默认为第一页
                            )
+        
         tmp_data_lst=[]
         json_data=json.loads(res_data)
-        web.cache.product_count=json_data['productCount']
+        if not json_data.get('success',None):
+            return "调用api.findProductInfoListQuery接口失败"
+        web.cache.product_count= ['productCount']
         web.cache.total_page=json_data['totalPage']
         tmp_data_lst.extend(json_data['aeopAEProductDisplayDTOList'])
         
         for i in range(2,web.cache.total_page+1):
             page_data=get_alidata_by_api(
                                "api.findProductInfoListQuery",
-                                access_token,
+                                self.access_token,
                                 productStatusType="onSelling",
                                 currentPage=i
                                )
             tmp_data_lst.extend(json.loads(page_data)['aeopAEProductDisplayDTOList'])
         
+
         products_lst=[]
         coll=getattr(web.ctx.dbcontext,MONGODB['DB_SMT_COLL'])
         
@@ -239,16 +297,37 @@ class SMTProducts(object):
         for item in tmp_data_lst:
             temp={}
             p_id=item['productId']
-
-            if not coll.find({'smt_productId':p_id}):
-                    
+            if not coll.find_one({'smt_productId':p_id}):
                 p_info=get_alidata_by_api(
                                        "api.findAeProductById",
-                                       access_token,
+                                       self.access_token,
                                        productId=p_id
                                        )
+                p_info_json=json.loads(p_info)
+                if not json_data.get('success',None):
+                    return "调用api.findAeProductById 接口失败"
+                
                 temp['smt_productId']=p_id
-                temp['smt_productSKUs']=json.loads(p_info)['aeopAeProductSKUs']
+                temp['image_url']=p_info_json['imageURLs'].split(';')[0]
+                temp['smt_productSKUs']=p_info_json['aeopAeProductSKUs']
+                
+                cateid=p_info_json['categoryId']
+                atrrs=self.get_attr_by_cateid(cateid)
+                if not atrrs.get('success',None):
+                    return "调用api.getAttributesResultByCateId 接口失败"
+                
+                atrrs_lst=atrrs['attributes']
+                
+                smt_skus=temp['smt_productSKUs']
+                
+                for item in smt_skus:
+                    if item['aeopSKUProperty']:
+                        for sku_p in item['aeopSKUProperty']:
+                            skuPropertyIdName_en,propertyValues=self.get_skuPropertyId_Name(atrrs_lst,sku_p['skuPropertyId'])
+                            propertyValueIdName_en=self.get_propertyValueId_Name(propertyValues,sku_p['propertyValueId'])
+                            sku_p['skuPropertyIdName_en']=skuPropertyIdName_en
+                            sku_p['propertyValueIdName_en']=propertyValueIdName_en
+                
     
                 coll.insert(temp)
                 products_lst.append(temp)
@@ -257,6 +336,52 @@ class SMTProducts(object):
             
         web.header('Content-type', 'text/html;charset=utf-8')
         return  products_lst
+    
+    def get_attr_by_cateid(self,cateid):
+        '''根据产品的cateId获取属性值'''
+        if web.cache.cate_attr_datas.get(cateid,None):
+            return web.cache.cate_attr_datas[cateid]
+        
+        
+        res_data=get_alidata_by_api(
+                           "api.getAttributesResultByCateId",
+                            self.access_token,
+                            cateId=cateid,
+                           )
+        json_data=json.loads(res_data)
+        
+        web.cache.cate_attr_datas[cateid]=json_data
+        return json_data
+    
+    def get_skuPropertyId_Name(self,attr_lst,skuPropertyId):
+        '''
+                        根据库存属性id从属性列表中取出相应的skuPropertyIdName_en和propertyValues列表
+        '''
+        if web.cache.sku_properid_datas.get(skuPropertyId,None):
+            return web.cache.sku_properid_datas[skuPropertyId]
+        
+        for item in attr_lst:
+            if item['id']==skuPropertyId:
+                skuPropertyIdName_en=item['names']['en']
+                if item.get('values',None):
+                    propertyValues=item['values']
+                break
+            
+        web.cache.sku_properid_datas[skuPropertyId]=(skuPropertyIdName_en,propertyValues)
+        return (skuPropertyIdName_en,propertyValues)
+            
+    def get_propertyValueId_Name(self,propertyValues,propertyValueId):
+        if web.cache.sku_properval_datas.get(propertyValueId,None):
+            return web.cache.sku_properval_datas[propertyValueId]
+        
+        for item in propertyValues:
+            if propertyValueId==item['id']:
+                propertyValueIdName_en=item['names']['en']
+                web.cache.sku_properval_datas[propertyValueId]=propertyValueIdName_en
+                return propertyValueIdName_en
+        
+    
+
 
 
 
@@ -277,7 +402,7 @@ application=app.wsgifunc()
 if __name__=="__main__":
     app.run()
 #    WSGIServer(('0.0.0.0', 8080), application).serve_forever()
-    
+#    print imageUrlFilter("http://img.vip.alibaba.com/img/wsproduct/sku/20/43/86/21/2043862125_175.jpg?1414551993115")
     
     
     
