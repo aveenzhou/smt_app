ed7f2e3e2c81f8ea957a454b50c08f7f2c3ce65f
aveenzhou <aveenzhou@gmail.com>
2014-11-09 18:22:52 +0800

    add new code

--- smt_app/main.py
+++ smt_app/main.py
@@ -27,7 +27,10 @@ render._lookup.filters['imageUrlFilter']=imageUrlFilter
         
 web.config.alibba_auth_url=get_alibba_auth_url()
 
-
+web.cache=web.Storage()
+web.cache.cate_attr_datas=web.storage()
+web.cache.sku_properid_datas=web.storage()
+web.cache.sku_properval_datas=web.storage()
 ########################################
 
 def init_dbcontext():
@@ -59,6 +62,7 @@ urls=(
       '/index','Index',
       '/update_link','UpdateLink',
       '/stock_update','StockUpdate',
+      '/product_update','ProductDate',
       '/login','Login',
       '/logout','Logout',
       '/test','Test'
@@ -295,9 +299,139 @@ class StockUpdate(object):
             return json.dumps({"msg":"更新成功","status":True,"productid":productid})
         except Exception,e:
             return json.dumps({"msg":"更新失败:%s" % str(e),"status":False,"productid":productid})
+        
+        
 
+class ProductDate(object):
+    def GET(self):
+        if not web.config.get('token_data',None) or web.config.token_data.get('error',None):
+            return json.dumps({"msg":"更新产品需要授权","status":False,'ali_auth_url':web.config.alibba_auth_url})
+        self.access_token=web.config.token_data.access_token
+        inputs=web.input()
+        productid=inputs.get("productId",None)
+    
+        if not productid:
+            return json.dumps({"msg":"参数错误","status":False})
+        
+        try:
+            productid=int(productid)
+            p_info=self._get_product_byid(productid)
+            
+            if not p_info.get('success',None):
+                return self.get_error_msg(p_info,productid)
+            
+            temp={}
+            temp['smt_productId']=productid
+            temp['image_url']=p_info['imageURLs'].split(';')[0]
+            temp['smt_productSKUs']=p_info['aeopAeProductSKUs']
+            
+            cateid=p_info['categoryId']
+            atrrs=self._get_attr_by_cateid(cateid)
+            if not atrrs.get('success',None):
+                return self.get_error_msg(atrrs,productid)
+            
+            atrrs_lst=atrrs['attributes']
+            
+            smt_skus=temp['smt_productSKUs']
+            
+            for item in smt_skus:
+                if item['aeopSKUProperty']:
+                    for sku_p in item['aeopSKUProperty']:
+                        skuPropertyIdName_en,propertyValues=self._get_skuPropertyId_Name(atrrs_lst,sku_p['skuPropertyId'])
+                        propertyValueIdName_en=self._get_propertyValueId_Name(propertyValues,sku_p['propertyValueId'])
+                        sku_p['skuPropertyIdName_en']=skuPropertyIdName_en
+                        sku_p['propertyValueIdName_en']=propertyValueIdName_en
+            
+            tbdata=self._get_taobaolinkbyId(productid)
+            if isinstance(tbdata, list) and tbdata:
+                link=tbdata[0].get('detailUrl',None)
+                temp['taobao_link']= link
+            
+            coll=getattr(web.ctx.dbcontext,MONGODB['DB_SMT_COLL'])
+            is_exist=coll.find_one({'smt_productId':productid})
+            if not is_exist:
+                coll.insert(temp)
+#                print "Get a Data:",temp
+                return json.dumps({"msg":"获取成功","status":True,"productid":productid})
+            else:
 
+                coll.update({'smt_productId':productid},{'$set':temp})
+#                print "Update a Data:",temp
+                return json.dumps({"msg":"更新成功","status":True,"productid":productid})
+        except Exception,e:
+            return json.dumps({"msg":"操作失败:%s" % str(e),"status":False,"productid":productid})
+        
+    def _get_product_byid(self,p_id):
+        
+        p_info=get_alidata_by_api(
+                               "api.findAeProductById",
+                               self.access_token,
+                               productId=p_id
+                               )
+        json_data=json.loads(p_info)
 
+        return json_data
+    
+    def _get_attr_by_cateid(self,cateid):
+        '''根据产品的cateId获取属性值'''
+        if web.cache.cate_attr_datas.get(cateid,None):
+            return web.cache.cate_attr_datas[cateid]
+        
+        
+        res_data=get_alidata_by_api(
+                           "api.getAttributesResultByCateId",
+                            self.access_token,
+                            cateId=cateid,
+                           )
+        json_data=json.loads(res_data)
+        
+        web.cache.cate_attr_datas[cateid]=json_data
+        return json_data
+        
+    def _get_skuPropertyId_Name(self,attr_lst,skuPropertyId):
+        '''
+                        根据库存属性id从属性列表中取出相应的skuPropertyIdName_en和propertyValues列表
+        '''
+        if web.cache.sku_properid_datas.get(skuPropertyId,None):
+            return web.cache.sku_properid_datas[skuPropertyId]
+        
+        for item in attr_lst:
+            if item['id']==skuPropertyId:
+                skuPropertyIdName_en=item['names']['en']
+                if item.get('values',None):
+                    propertyValues=item['values']
+                break
+            
+        web.cache.sku_properid_datas[skuPropertyId]=(skuPropertyIdName_en,propertyValues)
+        return (skuPropertyIdName_en,propertyValues)
+            
+    def _get_propertyValueId_Name(self,propertyValues,propertyValueId):
+        if web.cache.sku_properval_datas.get(propertyValueId,None):
+            return web.cache.sku_properval_datas[propertyValueId]
+        
+        for item in propertyValues:
+            if propertyValueId==item['id']:
+                propertyValueIdName_en=item['names']['en']
+                web.cache.sku_properval_datas[propertyValueId]=propertyValueIdName_en
+                return propertyValueIdName_en
+        
+    def _get_taobaolinkbyId(self,p_id):
+        res_data=get_alidata_by_api(
+                           "api.listTbProductByIds",
+                            self.access_token,
+                            productIds=p_id,
+                           )
+        json_data=json.loads(res_data)
+        return json_data
+
+    
+    def get_error_msg(self,error_data,productid):
+        error= error_data['error_message'] if error_data.get('error_message',None) else error_data.get('exception','')
+        error_data={"msg":"操作失败:%s" % str(error),"status":False,"productid":productid}
+        if error_data.get('error_code',None)=='401':
+            error_data['ali_auth_url']=web.config.alibba_auth_url
+        return json.dumps(error_data)
+    
 def loadhook():
     web.header('Content-type', "text/html; charset=utf-8")
     try:
